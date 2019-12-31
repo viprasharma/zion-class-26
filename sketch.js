1705513fc6ab1296b7c0cee28d3dc0f1470431f6
vipra <vipra.sharma06@gmail.com>
2019-12-31 15:28:12 +0530

    done

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ const World= Matter.World;
 const Bodies = Matter.Bodies;
 
 var engine, world;
-var box1, pig1;
+var box1, pig1,plateform;
 var backgroundImg;
 
 function preload() {
@@ -16,8 +16,8 @@ function setup(){
     world = engine.world;
 
     
-    ground = new Ground(600,height,1200,20)
-
+    ground = new Ground(600,height,1200,20);
+    plateform = new Ground(150,305,300,170);
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
@@ -59,4 +59,5 @@ function draw(){
     log5.display();
 
     bird.display();
+    plateform.display();
 }
